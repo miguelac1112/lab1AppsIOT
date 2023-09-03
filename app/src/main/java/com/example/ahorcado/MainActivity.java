c4671084796e86a774323b59483057e3b02d8b6b
Miguel <a20190050@pucp.edu.pe>
2023-09-03 14:36:39 -0500

    Context Menu owo

--- app/src/main/java/com/example/ahorcado/MainActivity.java
+++ app/src/main/java/com/example/ahorcado/MainActivity.java
@@ -1,52 +1,61 @@
 package com.example.ahorcado;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat; // Importa la clase ContextCompat
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView titleTextView;
+    private int currentTextColor; // Almacena el color actual del texto
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View titleTextView = findViewById(R.id.textView2);
+        titleTextView = findViewById(R.id.textView2);
+        currentTextColor = ContextCompat.getColor(this, android.R.color.black); // Color inicial
+        titleTextView.setTextColor(currentTextColor);
+
         registerForContextMenu(titleTextView);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu, v, menuInfo);
         getMenuInflater().inflate(R.menu.menu_context,menu);
+    }
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        String title = item.getTitle().toString(); // Obtiene el título del elemento de menú
+
+        if ("Azul".equals(title)) {
+            currentTextColor = ContextCompat.getColor(this, R.color.blue_color);
+        } else if ("Verde".equals(title)) {
+            currentTextColor = ContextCompat.getColor(this, R.color.green_color);
+        } else if ("Rojo".equals(title)) {
+            currentTextColor = ContextCompat.getColor(this, R.color.red_color);
+        }
+
+        titleTextView.setTextColor(currentTextColor);
         return true;
     }
 
-    //Metodo para asignar las funciones correspondientes a las opciones
-    @Override
-    public boolean onContextItemSelected(@NonNull MenuItem item) {
-        TextView titleTextView = findViewById(R.id.textView2);
 
-        switch (item.getItemId()) {
-            case 100: // Valor constante para color azul
-                titleTextView.setTextColor(getResources().getColor(android.R.color.holo_blue_dark));
-                return true;
-            case 101: // Valor constante para color verde
-                titleTextView.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
-                return true;
-            case 102: // Valor constante para color rojo
-                titleTextView.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
-                return true;
-            default:
-                return super.onContextItemSelected(item);
-        }
-    }
+
 
     public void play(View view){
         Intent intent = new Intent(this, GameActivity.class);
         startActivity(intent);
     }
 }
+
